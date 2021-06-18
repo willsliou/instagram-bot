ca4c081086cf310e259fda27c858c59d5836bcde
Wills Liou <willsliou@protonmail.com>
2021-06-18 01:31:25 -0700

    Added click to inject.js to click on first photo

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,7 +3,13 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 		console.log(window.location.hash);
-		console.log("Automate");
+
+		// Automate only if url has #automate
+		if (window.location.hash === '#automate') {
+			console.log('Automating!');
+			// Click on first photo
+			document.querySelectorAll('.v1Nh3 > a')[0].click()
+		}
 		
         // const elem = document.querySelector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
 
