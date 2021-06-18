2c2dc11bc8bb8390c2d60850cc66da6ea54a10c8
Wills Liou <willsliou@protonmail.com>
2021-06-18 00:31:14 -0700

    Change matches object in manifest.json to incldue instagram url instead of google url

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,15 +3,11 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 		console.log(window.location.hash);
+		console.log("Automate");
 		
-		// // ----------------------------------------------------------
-		// // This part of the script triggers when page is done loading
-		// console.log("ALOOOHAAA,This message was sent from scripts/inject.js... SO STRONG!");
-		// // ----------------------------------------------------------
+        // const elem = document.querySelector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
 
-        const elem = document.querySelector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
-
-        elem.value = 'My Search';
+        // elem.value = 'My Search';
 
 	}
 	}, 10);
