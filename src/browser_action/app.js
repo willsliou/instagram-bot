a64a3e8717bf8443f11ff7618ab11525c410e41b
Wills Liou <willsliou@protonmail.com>
2021-06-17 17:54:13 -0700

    Add in content script and #automate url

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,5 +1,5 @@
 document.getElementById('automate-form').onsubmit = event => {
     event.preventDefault();
     const hashtag = document.getElementById('hashtag').value;
-    chrome.tabs.create( { url: `https://www.instagram.com/explore/tags/${hashtag}` });
+    chrome.tabs.create( { url: `https://www.instagram.com/explore/tags/${hashtag}/#automate` });
 }
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,8 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
+		console.log(window.location.hash);
+		
 		// // ----------------------------------------------------------
 		// // This part of the script triggers when page is done loading
 		// console.log("ALOOOHAAA,This message was sent from scripts/inject.js... SO STRONG!");
